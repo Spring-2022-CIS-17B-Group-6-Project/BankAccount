53c6200fbe7595187929c36ee6202fa4382d1042
PhysicsJoe92 <hernandezj478@yahoo.com>
2022-06-05 18:14:00 -0700

    Update BankAccount.cpp

--- BankAccount.cpp
+++ BankAccount.cpp
@@ -22,25 +22,25 @@ int main(int argv, char** argc){
     // Display Main Menu
     MainMenu mainMenu;
     
-
-        try{
-            mMenuChoice = mainMenu.getOptions();
-            if(mMenuChoice!=1 && mMenuChoice !=2 && mMenuChoice != 3 
-                    && mMenuChoice != 4 && mMenuChoice != 5){
-                throw mMenuChoice;
-            }
-        }catch(int e){
-            cout<<"ERROR: Incorrect Selection. Exiting Program..."<<endl;
-            return 0;
+    // Display Main Menu and get user input
+    try{
+        mMenuChoice = mainMenu.getOptions();
+        if(mMenuChoice!=1 && mMenuChoice !=2 && mMenuChoice != 3 
+                && mMenuChoice != 4 && mMenuChoice != 5){
+            throw mMenuChoice;
         }
+    }catch(int e){
+        cout<<"ERROR: Incorrect Selection. Exiting Program..."<<endl;
+        return 0;
+    }
     
-    
+    // Branch off to each case for Main Menu
     switch(mMenuChoice){
         case 1: cout<<"Open Account Menu..."<<endl;break;// Open Account Menu
         case 2: cout<<"Deposit Menu"<<endl;break;// Deposit to account menu
         case 3: cout<<"Withdraw Menu"<<endl;break;// Withdraw from account menu
         case 4: cout<<"Print Transactions Menu"<<endl;break;// Print Transactions
-        default: cout<<"Do nothing... Exiting..."<<endl;// Do nothing to end application(goto?)
+        default: cout<<"Do nothing... Exiting..."<<endl;// Do nothing to end application
     }
     
     return 0;
